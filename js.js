7bde15f627e51051011f0cd1056381f356886f91
Jessie Chen <jesch136@student.liu.se>
2016-04-26 22:05:00 +0200

    projekt layout

--- js.js
+++ js.js
@@ -1,10 +1,3 @@
 /**
  * Created by jers on 26/04/16.
  */
-$(window).on('scroll', function(){
-	$timeline_block.each(function(){
-		if( $(this).offset().top <= $(window).scrollTop()+$(window).height()*0.75 && $(this).find('.cd-timeline-img').hasClass('is-hidden') ) {
-			$(this).find('.cd-timeline-img, .cd-timeline-content').removeClass('is-hidden').addClass('bounce-in');
-		}
-	});
-});
