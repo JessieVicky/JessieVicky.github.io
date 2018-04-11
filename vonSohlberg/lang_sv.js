89a7ca2c5eda17ce334c54d7269ee121aa82b555
JessieVicky <jesch136@student.liu.se>
2018-04-11 12:27:03 +0200

    Update lang_sv.js

--- vonSohlberg/lang_sv.js
+++ vonSohlberg/lang_sv.js
@@ -9,7 +9,7 @@ var sve = {
 		"dresscode" : "Dresscode",
 		"contact" : "Kontakt",
 		"intro" : "Bröllopet närmar sig med stormsteg och vi ser jättemycket fram emot att ni ska komma och dela vår dag med oss! På denna sida kan ni hitta all viktig information om dagen. Tanken är att all information ni behöver ska finnas tillgänglig på denna hemsida. Nedan följer viktig information. Vi behöver att ni"
-		"intro_br" : "senast 30e April meddelar oss:",
+		"intro_highlight" : "senast 30e April meddelar oss:",
 		"pt1" : "Om ni tar er ut själva eller vill åka med buss från Bro Station 15:00",
 		"pt2" : "Om ni vill bo kvar tillsammans med oss på Görvälns Slott efteråt och hur många enkel/dubbelrum ni behöver (och då åker vi tillsammans med buss efter festens slut)",
 		"pt3" : "Eventuella allergier",
