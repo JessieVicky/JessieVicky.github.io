88de6086cbb391f97b5873462cb19f7d6edcbc88
JessieVicky <jesch136@student.liu.se>
2018-04-11 12:24:48 +0200

    Update lang_sv.js

--- vonSohlberg/lang_sv.js
+++ vonSohlberg/lang_sv.js
@@ -1,7 +1,17 @@
 
 var sve = { 
 		"banner" :"Nu var det dags!",
-		"navigation" : "Navigering",
-		"contents" : "Innehåll",
-		"test" : "testaaa"
+		"home" : "Home",
+		"schedule" : "Schema",
+		"transport" : "Transport",
+		"food" : "Dryck och Allergier",
+		"accomodation" : "Boende",
+		"dresscode" : "Dresscode",
+		"contact" : "Kontakt",
+		"intro" : "Bröllopet närmar sig med stormsteg och vi ser jättemycket fram emot att ni ska komma och dela vår dag med oss! På denna sida kan ni hitta all viktig information om dagen. Tanken är att all information ni behöver ska finnas tillgänglig på denna hemsida. Nedan följer viktig information. Vi behöver att ni"
+		"intro_br" : "senast 30e April meddelar oss:",
+		"pt1" : "Om ni tar er ut själva eller vill åka med buss från Bro Station 15:00",
+		"pt2" : "Om ni vill bo kvar tillsammans med oss på Görvälns Slott efteråt och hur många enkel/dubbelrum ni behöver (och då åker vi tillsammans med buss efter festens slut)",
+		"pt3" : "Eventuella allergier",
+		"pt4" : "Om ni främst dricker alkoholfritt eller alkoholhaltigt (man får dricka av båda alternativen men för kökets skull behöver ni meddela vilket ni föredrar mest)"
 	}
